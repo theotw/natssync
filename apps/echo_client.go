a392a6277a1b22200da57c4aeaa3162c3574e82d
John Mears <jmears@netapp.com>
2021-02-03 11:40:03 -0700

    Refactor echo client

--- apps/echo_client.go
+++ apps/echo_client.go
@@ -1,53 +1,75 @@
 /*
- * Copyright (c) The One True Way 2021. Apache License 2.0. The authors accept no liability, 0 nada for the use of this software.  It is offered "As IS"  Have fun with it!!
+ * Copyright (c) The One True Way 2021. Apache License 2.0.
+ * The authors accept no liability, 0 nada for the use of this software.
+ * It is offered "As IS"  Have fun with it!!
  */
 
 package main
 
 import (
+	"flag"
 	"fmt"
-	"github.com/google/uuid"
-	"github.com/nats-io/nats.go"
-	"github.com/theotw/natssync/pkg"
-	"github.com/theotw/natssync/pkg/msgs"
 	"log"
-	"os"
 	"strings"
 	"time"
+
+	"github.com/google/uuid"
+	"github.com/nats-io/nats.go"
+
+	"github.com/theotw/natssync/pkg"
+	"github.com/theotw/natssync/pkg/msgs"
 )
 
-//Echo is a secial message type.
+type Arguments struct {
+	message *string
+	clientID *string
+	natsURL *string
+}
+
+func getArguments() Arguments {
+	args := Arguments{
+		flag.String("msg", "", "Message to send to the client"),
+		flag.String("id", pkg.Config.PremId, "ID of the receiving client"),
+		flag.String("url", pkg.Config.NatsServerUrl, "URL to connect to NATS"),
+	}
+	flag.Parse()
+	return args
+}
+
+//Echo is a special message type.
 //its the only message the system looks at and responds to.
-//Instead of a single reply, it is expecting multiple replies.  All the replies will have the "reply" subject as its root, and then a last string
+//Instead of a single reply, it is expecting multiple replies.
+//All the replies will have the "reply" subject as its root, and then a last string
 //that indicates which part of the journey has been hit.
-// the loop ends when it sees echolet.
+//the loop ends when it sees echolet.
 func main() {
-	natsURL := pkg.Config.NatsServerUrl
-	fmt.Printf("Using NATS Server %s \n", natsURL)
-	nc, err := nats.Connect(natsURL)
+	args := getArguments()
+
+	fmt.Printf("Using NATS Server %s \n", *args.natsURL)
+	nc, err := nats.Connect(*args.natsURL)
 	if err != nil {
 		log.Fatal(err)
 	}
-	text := "Hello NATS"
-	if len(os.Args) < 3 {
-		panic("Must have 2 arguments <target localtionID> <message>")
-	}
 	defer nc.Close()
+
 	randomClientUUID := uuid.New().String()
 	reply := fmt.Sprintf("%s.%s.%s", msgs.NB_MSG_PREFIX, msgs.CLOUD_ID, randomClientUUID)
-	clientID := os.Args[1]
-	text = os.Args[2]
-	sub := fmt.Sprintf("%s.%s.%s", msgs.SB_MSG_PREFIX, clientID, msgs.ECHO_SUBJECT_BASE)
+
+	sub := fmt.Sprintf("%s.%s.%s", msgs.SB_MSG_PREFIX, *args.clientID, msgs.ECHO_SUBJECT_BASE)
 	replyListenSub := fmt.Sprintf("%s.%s.%s.*", msgs.NB_MSG_PREFIX, msgs.CLOUD_ID, randomClientUUID)
 	sync, err := nc.SubscribeSync(replyListenSub)
-
-	nc.PublishRequest(sub, reply, []byte(text))
-	nc.Flush()
 	if err != nil {
-		fmt.Printf("Got Error %s", err.Error())
-		os.Exit(1)
+		log.Fatalf("Error subscribing: %e", err)
 	}
-	for true {
+
+	if err = nc.PublishRequest(sub, reply, []byte(*args.message)); err != nil {
+		log.Fatalf("Error publishing message: %e", err)
+	}
+	if err = nc.Flush(); err != nil {
+		log.Fatalf("Error flushing NATS connection: %e", err)
+	}
+
+	for {
 		msg, err := sync.NextMsg(5 * time.Minute)
 		if err != nil {
 			fmt.Printf("Got Error %s \n", err.Error())
@@ -58,6 +80,5 @@ func main() {
 				break
 			}
 		}
-
 	}
 }
