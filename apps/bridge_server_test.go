d833d75be02d730be3e1582639b904f062496eea
John Mears <jmears@netapp.com>
2021-03-17 14:55:34 -0600

    Fix bridge server test and switch to mongo for keystore

--- apps/bridge_server_test.go
+++ apps/bridge_server_test.go
@@ -5,31 +5,11 @@
 package main
 
 import (
-	"fmt"
-	log "github.com/sirupsen/logrus"
-	"github.com/theotw/natssync/pkg"
-	"github.com/theotw/natssync/pkg/cloudserver"
 	"testing"
+
+	"github.com/theotw/natssync/pkg/cloudserver"
 )
 
 func Test_main(t *testing.T) {
-	logLevel := pkg.GetEnvWithDefaults("LOG_LEVEL", "debug")
-
-	level, levelerr := log.ParseLevel(logLevel)
-	if levelerr != nil {
-		log.Infof("No valid log level from ENV, defaulting to debug level was: %s", level)
-		level = log.DebugLevel
-	}
-	log.SetLevel(level)
-
-	fmt.Println("Init Cache Mgr")
-	err := cloudserver.InitCacheMgr()
-	if err != nil {
-		log.Fatalf("Unable to initialize the cache manager %s", err.Error())
-	}
-	go cloudserver.RunMsgHandler("astra.>")
-	fmt.Println("Starting Server")
-	cloudserver.RunBridgeServer(true)
-	fmt.Println("Server stopped")
-
+	cloudserver.RunBridgeServerApp(false)
 }
