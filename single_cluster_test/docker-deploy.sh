62df9ada4a1f8ebc10874c841fc88125c36be12e
John Mears <jmears@netapp.com>
2021-03-18 12:40:32 -0600

    Fix log collection

--- single_cluster_test/docker-deploy.sh
+++ single_cluster_test/docker-deploy.sh
@@ -14,13 +14,13 @@ CLOUD_BRIDGE_URL="http://$SYNCSERVER:8080"
 docker network create $NETWORK
 
 # Cloud side
-docker run -d --network $NETWORK --name $NATSCLOUD -p 4222:4222 nats
 docker run -d --network $NETWORK --name $MONGOCLOUD mongo
+docker run -d --network $NETWORK --name $NATSCLOUD -p 4222:4222 nats
 docker run -d --network $NETWORK --name $SIMPLEAUTH -e NATS_SERVER_URL=$CLOUDNATSURL $REPO/$SIMPLEAUTH
 docker run -d --network $NETWORK --name $SYNCSERVER -p 8080:8080 -e KEYSTORE_URL=$CLOUDKEYSTOREURL -e NATS_SERVER_URL=$CLOUDNATSURL $REPO/$SYNCSERVER
 
 # On-prem side
-docker run -d --network $NETWORK --name $NATSONPREM nats
 docker run -d --network $NETWORK --name $MONGOONPREM mongo
+docker run -d --network $NETWORK --name $NATSONPREM nats
 docker run -d --network $NETWORK --name $ECHOPROXY -e NATS_SERVER_URL=$ONPREMNATSURL $REPO/$ECHOPROXY
 docker run -d --network $NETWORK --name $SYNCCLIENT -p 8081:8080 -e KEYSTORE_URL=$ONPREMKEYSTOREURL -e NATS_SERVER_URL=$ONPREMNATSURL -e CLOUD_BRIDGE_URL=$CLOUD_BRIDGE_URL $REPO/$SYNCCLIENT
--- single_cluster_test/docker-logs.sh
+++ single_cluster_test/docker-logs.sh
@@ -6,14 +6,15 @@ source "$(dirname $0)/docker-containers.sh"
 
 if [ -z "$1" ]
 then
-  echo "Please provide a log file name"
+  echo "Please provide a log directory name"
   exit 1
 fi
 
-logfile=$1
+logsDir=$1
 
-for container in $CONTAINERS
+mkdir -p "$logsDir"
+
+for container in ${CONTAINERS[*]}
 do
-  echo "==========$container==========" >> "$logfile"
-  docker logs "$container" >> "$logfile"
+  docker logs "$container" >> "${logsDir}/${container}.txt" 2>&1
 done
