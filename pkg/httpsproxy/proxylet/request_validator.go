8adcd4305f759a91ffe6a221f8d177cbc90e2104
Vivin Wilson <vivin.wilson@netapp.com>
2021-12-08 18:42:53 -0500

    updating based on comments

--- pkg/httpsproxy/proxylet/request_validator.go
+++ pkg/httpsproxy/proxylet/request_validator.go
@@ -14,13 +14,20 @@ import (
 
 const (
 	defaultConfigFilePath = "/etc/proxylet/validHostsConfig.json"
+	defaultBehaviorBlock  = "block"
+	defaultBehaviorAllow  = "allow"
 )
 
+type configFileData struct {
+	Default string          `json:"defaultBehaviour"`
+	Hosts   map[string]bool `json:"hosts"`
+}
+
 type requestValidator struct {
+	sync.RWMutex
 	configFilePath string
-	sync.Mutex
-	configMap   map[string]bool
-	lastUpdated time.Time
+	configMap      configFileData
+	lastUpdated    time.Time
 }
 
 func NewRequestValidator(configFilePaths ...string) (*requestValidator, error) {
@@ -56,28 +63,30 @@ func (rv *requestValidator) swapMapIfRequired() error {
 		return fmt.Errorf("listing file failed with error: %v", err)
 	}
 
-	return func() error {
-		rv.Mutex.Lock()
-		defer rv.Mutex.Unlock()
+	if rv.lastUpdated.IsZero() || fileInfo.ModTime().After(rv.lastUpdated) {
 
-		if rv.lastUpdated.IsZero() || fileInfo.ModTime().After(rv.lastUpdated) {
+		newConfigFileData := configFileData{}
+		byteData, err := ioutil.ReadFile(rv.configFilePath)
+		if err != nil {
+			return fmt.Errorf("failed to read file '%v': %v", rv.configFilePath, err)
+		}
 
-			newConfigFileData := make(map[string]bool)
-			byteData, err := ioutil.ReadFile(rv.configFilePath)
-			if err != nil {
-				return fmt.Errorf("failed to read file '%v': %v", rv.configFilePath, err)
-			}
+		if err = json.Unmarshal(byteData, &newConfigFileData); err != nil {
+			return fmt.Errorf("failed to unmarshal data in file '%v': %v", rv.configFilePath, err)
+		}
 
-			if err = json.Unmarshal(byteData, &newConfigFileData); err != nil {
-				return fmt.Errorf("failed to unmarshal data in file '%v': %v", rv.configFilePath, err)
-			}
+		func() {
+			rv.RWMutex.Lock()
+			defer rv.RWMutex.Unlock()
 
 			rv.configMap = newConfigFileData
 			rv.lastUpdated = fileInfo.ModTime()
-		}
+		}()
 
-		return nil
-	}()
+	}
+
+	return nil
+
 }
 
 func getTargetHost(target string) string {
@@ -96,14 +105,23 @@ func (rv *requestValidator) IsValidRequest(target string) error {
 
 	targetExists := false
 	targetValue := false
+	defaultBehavior := defaultBehaviorBlock
 
 	func() {
-		rv.Mutex.Lock()
-		defer rv.Mutex.Unlock()
-		targetValue, targetExists = rv.configMap[targetHost]
+		rv.RWMutex.RLock()
+		defer rv.RWMutex.RUnlock()
+
+		targetValue, targetExists = rv.configMap.Hosts[targetHost]
+
+		defaultBehaviourTmp := rv.configMap.Default
+		if defaultBehaviourTmp != "" {
+			defaultBehavior = defaultBehaviourTmp
+		}
+
 	}()
 
-	if !targetExists || !targetValue {
+	if (!targetExists || !targetValue) && defaultBehavior != defaultBehaviorAllow {
+
 		return fmt.Errorf("invalid target")
 	}
 
