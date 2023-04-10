def24d177188e901e801dd9f22390d301e6e51b9
bmason <briab@gabey.com>
2023-04-10 15:49:33 -0500

    left some code in to allow use without the github secret as an ENV var

--- cicd_gen_certs.sh
+++ cicd_gen_certs.sh
@@ -1,7 +1,15 @@
 #This is for CICD workflow that has the key pulled from the secrets repo and store as an env var
 #Generates server keys and signs them with the CA
-base64 -d myca.c.b64 >myCA.pem
-echo $CA_KEY | base64 -d > myCA.key
+
+if [[ -z "${CA_KEY}" ]]; then
+  echo "No CA KEY ENV var, making one for testing"
+  openssl genrsa -out myCA.key 2048
+  openssl req -x509 -new -nodes -key myCA.key -sha256 -days 1825 -out myCA.pem
+else
+  echo "Using CA from Secrets"
+  base64 -d myca.c.b64 >myCA.pem
+  echo $CA_KEY | base64 -d > myCA.key
+fi
 openssl genrsa -out out/k8srelay.key 2048
 openssl req -new -key out/k8srelay.key -out out/k8srelay.csr  -subj /C=US/O=theOTW/OU=Engineering/CN=k8srelay
 openssl x509 -req -in out/k8srelay.csr -CA myCA.pem -CAkey myCA.key -CAcreateserial -out out/k8srelay.crt -days 825 -sha256 -extfile samplecerts/x509.config
