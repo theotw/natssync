0f08d77a5f856135653977d59739b49dd01095c4
bmason <briab@gabey.com>
2023-04-20 09:58:16 -0500

    updated check

--- cicd_gen_certs.sh
+++ cicd_gen_certs.sh
@@ -1,13 +1,18 @@
+#!/bin/bash
+
 #This is for CICD workflow that has the key pulled from the secrets repo and store as an env var
 #Generates server keys and signs them with the CA
 
-if [[ -z "${CA_KEY}" ]]; then
+if [[ -z ${CA_KEY} ]]; then
   echo "No CA KEY ENV var, making one for testing"
   openssl genrsa -out myCA.key 2048
   openssl req -x509 -new -nodes -key myCA.key -sha256 -days 1825 -out myCA.pem
 else
   echo "Using CA from Secrets"
+  #linux
   base64 -d myca.c.b64 >myCA.pem
+  #mac
+  #base64 -d -i myca.c.b64 -o myCA.pem
   echo $CA_KEY | base64 -d > myCA.key
 fi
 openssl genrsa -out out/k8srelay.key 2048
